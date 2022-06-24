7315cf89b836c27fe43a5041f4baaf17dee5f8a0
bkzareef <107204129+bkzareef@users.noreply.github.com>
2022-06-24 10:25:24 -0500

    Add files via upload

---
+++ stockfinder.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+from requests import exceptions as reqerr
+
+
+def find_price(stock):
+    if stock == "":
+        quit()
+    try:
+        url = 'https://finance.yahoo.com/quote/' + stock
+
+        # finds website of stock and gets its html
+        site = requests.get(url, timeout=3)
+        html = site.text
+
+        # creates soup object which allows for parsing functions and finds the element that holds the stock value
+        soup = BeautifulSoup(html, 'html.parser')
+        # finds fin-streamer tag with specific a specific class
+        fin = soup.findAll('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'})
+        if not fin:
+            print("The stock you entered does not exist.")
+            quit()
+        print(f"One {stock} share is worth: {fin[0].string}")
+    except reqerr.Timeout:
+        print("A timeout error occurred.")
+    except reqerr.ConnectionError:
+        print("There was an issue while reaching the website.")
+    finally:
+        quit()
+
+
+if __name__ == "__main__":
+    request = input("Please type the stock's ticker symbol in all caps. ")
+    find_price(request)
