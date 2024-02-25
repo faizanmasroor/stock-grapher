f29d608cd4937713e427177ed3286b3ce536d57f
Faizan Masroor (Post Oak) <faizanmasroor01@gmail.com>
2024-02-25 13:42:53 -0600

    Better divided try/except blocks by putting them outside function definitions and create a separate function to connect to the website to make error handling more intuitive.

--- main.py
+++ main.py
@@ -3,30 +3,37 @@ import requests
 from requests.exceptions import Timeout, ConnectionError
 
 
-def find_price(stock):
-    try:
-        url = 'https://finance.yahoo.com/quote/' + stock
+def conn_to_website(url):
+    site = requests.get(url, timeout=3)
+    return site.text
 
-        # finds website of stock and gets its html
-        site = requests.get(url, timeout=3)
-        html = site.text
 
-        # creates soup object which allows for parsing functions and finds the element that holds the stock value
-        soup = BeautifulSoup(html, 'html.parser')
-        # finds fin-streamer tag with specific a specific class
-        fin = soup.findAll('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'})
-        if not fin:
-            print("The stock you entered does not exist.")
-            quit()
-        print(f"One {stock} share is worth: {fin[0].string}")
-    except Timeout:
-        print("A timeout error occurred.")
-    except ConnectionError:
-        print("There was an issue while reaching the website.")
-    finally:
-        quit()
+def find_price(html, stock):
+    # creates soup object which allows for parsing functions and finds the element that holds the stock value
+    soup = BeautifulSoup(html, 'html.parser')
+    # finds fin-streamer tag with specific a specific class
+    fin = soup.findAll('fin-streamer', {'class': 'Fw(b) Fz(36px) Mb(-4px) D(ib)'})
+    print(f"One {stock} share is worth: {fin[0].string}\n")
 
 
 if __name__ == "__main__":
-    request = input("Enter the stock's ticker symbol in all caps: ")
-    find_price(request)
+    print("""Welcome to the stock price finder!\n""")
+
+    while True:
+        target_stock = input("Enter the stock's ticker symbol in all caps: ")
+        stock_url = 'https://finance.yahoo.com/quote/' + target_stock
+
+        try:
+            page_content = conn_to_website(stock_url)
+        except Timeout:
+            print("A timeout error occurred.\n")
+            continue
+        except ConnectionError:
+            print("There was an issue connecting to the website.\n")
+            continue
+
+        try:
+            find_price(page_content, target_stock)
+        except IndexError:
+            print("The stock you entered does not exist.\n")
+            continue
