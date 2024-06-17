cf56d29a79cdd7e41628238401948071b290f21e
Faizan Masroor (Post Oak) <faizanmasroor01@gmail.com>
2024-06-17 11:14:01 -0500

    Deleted test files.

--- main.py
+++ main.py
@@ -1,98 +1,75 @@
+from datetime import date, timedelta
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import yfinance as yf
 
-# String variables storing user input
-ticker = ""
-period = ""
-interval = ""
 
-# Constant; includes all valid boolean responses; period and interval parameters for yfinance functions
-bools = ["y", "n"]
-valid_periods = {
-    "1d": 9,
-    "5d": 10,
-    "1mo": 11,
-    "3mo": 12,
-    "6mo": 13,
-    "1y": 14,
-    "2y": 15,
-    "5y": 16,
-    "10y": 17,
-    "ytd": 18,
-    "max": 19
-}
-valid_intervals = {
-    "1m": 1,
-    "2m": 2,
-    "5m": 3,
-    "15m": 4,
-    "30m": 5,
-    "60m": 6,
-    "90m": 7,
-    "1h": 8,
-    "1d": 9,
-    "5d": 10,
-    "1wk": 10.5,
-    "1mo": 11,
-    "3mo": 12,
-}
+# Gets a stock's historical data and adds columns for specific time measures;
+# a Ticker object, period, and interval are passed as arguments; a Pandas DataFrame is returned
+def get_data(ticker: yf.Ticker, period, interval) -> pd.DataFrame:
+    hist = ticker.history(period=period, interval=interval)
+    hist['Timestamp'] = hist.index
+    hist['Minute'] = hist.Timestamp.dt.minute
+    hist['Hour'] = hist.Timestamp.dt.hour
+    hist['DecimalHour'] = hist.Timestamp.dt.hour  # This column is updated via hour_decimalize()
+    hist['Day'] = hist.Timestamp.dt.day
+    hist['Month'] = hist.Timestamp.dt.month
+    hist['Year'] = hist.Timestamp.dt.year
+    return hist
 
 
-def print_data(df: pd.Series):
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(df)
+# Removes undesired price columns in the DataFrame
+def filter_data(df: pd.DataFrame) -> pd.DataFrame:
+    df.drop(columns=["Open", "Low", "Close", "Volume", "Dividends", "Stock Splits"], inplace=True)
+    return df
 
 
-def plot_data(df: pd.Series):
-    # Plots stock prices as a graph and saves png files per user request;
-    save_req = ""
-    x = np.arange(len(df.index))
-    y = df.values
-    plt.plot(x, y)
-    plt.title(ticker)
-    plt.xlabel("Time")
-    plt.show(block=False)
-    while (not save_req) or (save_req not in bools):
-        save_req = input("Save graph in current directory? [y/n]: ").lower()
-    if save_req == "y":
-        plt.savefig(f"{ticker}.png")
-    plt.close()
+# Adds 0.5 to the Hour value in each row that has 30 as the Minutes value
+def hour_decimalize(df: pd.DataFrame) -> pd.DataFrame:
+    df['DecimalHour'] = np.where(df['Minute'] == 30, df['Hour'] + 0.5, df['Hour'])
+    return df
 
 
-if __name__ == "__main__":
-    print("Type the ticker symbol of the stock you wish to analyze.\n")
-    ticker = input(">>> ").upper()
+# Creates a today date for today's month and identifies the last trading day
+# TODO: Change yesterday calculation, because certain weekdays may be holidays (non-trading days)
+today = date.today()
+yesterday = today
+while yesterday.weekday() > 4:
+    yesterday -= timedelta(days=1)
 
-    # Prompts user for the period of analysis; re-prompts in the case of invalid inputs
-    print(f"\nChoose the period of analysis:\n{list(valid_periods.keys())}\n")
-    while True:
-        period = input(">>> ").lower()
-        if period in valid_periods:
-            break
-        print("Invalid period entered.")
+tk = yf.Ticker("TMUS")
 
-    # Same approach as above, but for the interval of analysis
-    print(f"\nChoose the interval of analysis:\n{list(valid_intervals.keys())}\n")
-    while True:
-        interval = input(">>> ").lower()
-        if interval in valid_intervals:
-            if valid_intervals[interval] < valid_periods[period]:
-                print("")
-                break
-            print("Please enter an interval less than the time period.")
-            continue
-        print("Invalid interval entered.")
+month_hist = get_data(tk, "3mo", "1d")
+day_hist = get_data(tk, "5d", "30m")
 
-    # Creates a Pandas DataFrame with historical data of all stocks (Series if just ones tock);
-    # filters out invalid stocks
-    ticker_obj = yf.Ticker(ticker)
-    hist = ticker_obj.history(period=period, interval=interval)
-    hist.reset_index(inplace=True)
-    if hist.empty:
-        print("Invalid ticker symbol.")
-        quit()
+month_hist = filter_data(month_hist)
+day_hist = filter_data(day_hist)
 
-    hist["Day of Week"] = hist["Date"].dt.dayofweek
-    print(hist)
+# Adjust DataFrames to only display data from the current month or yesterday, respectively
+month_hist = month_hist[month_hist['Month'] == today.month]
+day_hist = day_hist[day_hist['Day'] == yesterday.day]
+
+day_hist = hour_decimalize(day_hist)
+
+"""Only used for debugging."""
+# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+#     print(day_hist)
+
+# Creates the plots for the month's and last trading day's stock prices
+month_plot = sns.lineplot(x=month_hist.Day, y=month_hist.High)
+plt.title(f"{today.strftime('%B %Y')} TMUS Stock Price")
+plt.xlim(0, 32)
+plt.ylabel('High (USD)')
+plt.grid()
+plt.show()
+
+day_plot = sns.lineplot(x=day_hist.DecimalHour, y=day_hist.High)
+plt.title(f"{today.strftime('%B %d, %Y')}, TMUS Stock Price (Last Trading Day)")
+plt.xlim(9, 16)
+plt.xlabel('Hour')
+plt.ylabel('High (USD)')
+plt.grid()
+plt.show()
