c7ff42aa2f111860768275096e3e1e2de527c4a3
Faizan Masroor (Post Oak) <faizanmasroor01@gmail.com>
2024-06-19 09:12:48 -0500

    - Added comments - Changed parameter name

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import seaborn as sns
 import yfinance as yf
 
 today = date.today()
-tk = yf.Ticker("TMUS")
 
 
+# Retrieves a Pandas DataFrame with the financial history of a stock
 def get_data(ticker: yf.Ticker, period, interval) -> pd.DataFrame:
     hist = ticker.history(period=period, interval=interval)
     hist['Timestamp'] = hist.index
@@ -22,23 +22,29 @@ def get_data(ticker: yf.Ticker, period, interval) -> pd.DataFrame:
     return hist
 
 
+# Removes all finance-y columns other than the "High" column
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     df.drop(columns=["Open", "Low", "Close", "Volume", "Dividends", "Stock Splits"], inplace=True)
     return df
 
 
-def filter_data(df: pd.DataFrame, time) -> pd.DataFrame:
+# Filters the DataFrame passed as the argument such that it either includes only the last trading day (time_length =
+# "Day") or the entire month which the last trading day belongs (time_length = "Month")
+def filter_data(df: pd.DataFrame, time_length) -> pd.DataFrame:
     idx = -1
     while df.iloc[idx]['Day'] == today.day:
         idx -= 1
-    df = df[df[time] == df.iloc[idx][time]]
+    df = df[df[time_length] == df.iloc[idx][time_length]]
     return df
 
 
+# Changes the "DecimalHour" column so 9 hours and 30 minutes becomes 9.5 decimal hours (only used for the day DataFrame)
 def hour_decimalize(df: pd.DataFrame) -> pd.DataFrame:
     df['DecimalHour'] = np.where(df['Minute'] == 30, df['Hour'] + 0.5, df['Hour'])
     return df
 
+
+tk = yf.Ticker("TMUS")
 
 month_hist = get_data(tk, "3mo", "1d")
 day_hist = get_data(tk, "5d", "30m")
@@ -46,8 +52,8 @@ day_hist = get_data(tk, "5d", "30m")
 month_hist = clean_data(month_hist)
 day_hist = clean_data(day_hist)
 
-month_hist = filter_data(month_hist, time='Month')
-day_hist = filter_data(day_hist, time='Day')
+month_hist = filter_data(month_hist, time_length='Month')
+day_hist = filter_data(day_hist, time_length='Day')
 
 day_hist = hour_decimalize(day_hist)
 
